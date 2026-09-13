35fdd52c2212d5c112de2e49783ba2a17133884b
ديفيد 31201942 <UG_31201942@ics.tanta.edu.eg>
2026-09-14 00:22:02 +0300

    Add FakeAuthMiddleware and TestAuthController

---
+++ TurathApi/src/TestAuthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Turath.src.auth
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestAuthController : ControllerBase
+    {
+        [HttpGet("me")]
+        public IActionResult GetCurrentUser()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var role = User.FindFirstValue(ClaimTypes.Role);
+
+            return Ok(new
+            {
+                Message = "Fake Auth Working Successfully!",
+                UserId = userId,
+                Role = role
+            });
+        }
+    }
+}
