109256f34d2b645cfdd8f35c3bcfe83234d3c3b2
shahdkhader30677-star <shahdkhader30677@gmail.com>
2026-09-13 13:15:40 +0300

    Fix API controllers, models, and JSON circular reference

--- TurathApi/Data/AppDbContext.cs
+++ TurathApi/Data/AppDbContext.cs
@@ -1,6 +1,29 @@
-﻿namespace TurathApi.Data
+﻿using Microsoft.EntityFrameworkCore;
+using TurathApi.Models;
+
+namespace TurathApi.Data
 {
-    public class AppDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=RebookDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+            }
+        }
+
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
     }
 }
---
+++ TurathApi/Models/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿namespace TurathApi.Models
+{
+    using System;
+
+    public class OrderItem
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public Guid OrderId { get; set; }
+        public Order Order { get; set; }
+
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
+
